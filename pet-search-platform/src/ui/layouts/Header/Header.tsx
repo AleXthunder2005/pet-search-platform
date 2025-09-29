cd832e24111d0a72136d2be6b42b8b2423e76eb5
AleXthunder2005 <aleksandrnar2005@gmail.com>
2025-09-29 21:26:05 +0300

    feat: add Header

---
+++ pet-search-platform/src/ui/layouts/Header/Header.tsx
@@ -0,0 +1,47 @@
+import styles from "./styles/Header.module.scss";
+import { Icon } from "@components/Icon/Icon";
+import Button from "@components/Button/Button";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
+
+export const Header = () => {
+    return (
+        <header className={styles["header"]}>
+            <div className={styles["header__logo"]}>
+                <Icon name="petPaw" size={60} color="orange" />
+            </div>
+
+            <nav className={styles["header__nav"]}>
+                <ul className={styles["nav-list"]}>
+                    <li className={styles["nav-list__item"]}>
+                        <NavLink
+                            to="/"
+                            end
+                            className={({ isActive }) =>
+                                clsx(
+                                    styles["nav-list__item-link"],
+                                    isActive && styles["nav-list__item-link--active"]
+                                )
+                            }
+                        >
+                            Главная
+                        </NavLink>
+                    </li>
+                    <li className={styles["nav-list__item"]}>
+                        <a
+                            href="#pets"
+                            className={styles["nav-list__item-link"]}
+                        >
+                            Потеряшки
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className={styles["header__actions"]}>
+                <Button isPrimary={false}>Зарегистрироваться</Button>
+            </div>
+        </header>
+    );
+};
+
